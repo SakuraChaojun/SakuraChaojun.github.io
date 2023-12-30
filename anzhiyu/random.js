f6ae75e0a04b8932e6626700a348cc09425f9272
sakurachaojun <sakurachaojun@gmail.com>
2023-12-30 02:57:27 -0700

    Site updated: 2023-12-30 02:57:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/11/hello-world/","2023/12/18/psychopy/","2023/12/18/product/"];function toRandomPost(){
+var posts=["2023/12/30/algorithm/","2023/12/11/hello-world/","2023/12/18/psychopy/","2023/12/18/product/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
